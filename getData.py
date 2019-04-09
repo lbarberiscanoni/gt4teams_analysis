a42e1f5e9f98e8f794f0e0d1acbc3877a0c88770
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2019-04-09 14:50:08 -0400

    added the analysis for every other piece of data

--- getData.py
+++ getData.py
@@ -128,5 +128,240 @@ def hawkdove():
 				writer.writerow(ob.values())
 				i += 1
 
+def bos():
+	with open('bos.pickle', 'rb') as handle:
+		    b = pickle.load(handle)
 
-downloadData()
+	i = 0
+	with open('bos.csv', 'w') as f:
+		writer = csv.writer(f)
+
+		idList = [""]
+		for value in b:
+			ob = {}
+
+			ob["humanID"] = value["surveyID"]
+			ob["opponent"] = value["model"] 
+
+
+			if (ob["humanID"] not in idList):
+
+				idList.append(ob["humanID"])
+
+				if value["opponent"] == "Riley":
+					ob["condition"] = "human"
+				elif value["opponent"] == "AI": 
+					ob["condition"] = "ai"
+
+				gameState = json.loads(value["gameState"])
+
+
+
+				players = ["human", "AI"]
+				strategies = ["selfish", "altruistic"]
+				events = ["coordination", "non_coordination"]
+				turns = ["A", "B", "C"]
+
+				for turn in turns:
+					for strategy in strategies:
+						for player in players:
+							key = player + "_" + strategy +  "_" + turn
+							ob[key] = 0
+
+						#treat selfish coordination as when the human subject benefits from the AI
+						for event in events:
+							key = event + "_" + strategy + "_" + turn
+							ob[key] = 0
+
+				for turn, history in gameState.items():
+					moves = history.values()
+
+					for move in moves:
+						move_human = move[0]
+						move_AI = move[1]
+
+						if move == [0, 0]:
+							ob["human_selfish_" + turns[int(turn)]] += 1
+							ob["AI_altruistic_" + turns[int(turn)]] += 1
+							ob["coordination_selfish_" + turns[int(turn)]] += 1
+						elif move == [0, 1]:
+							ob["human_selfish_" + turns[int(turn)]] += 1
+							ob["AI_selfish_" + turns[int(turn)]] += 1
+							ob["non_coordination_selfish_" + turns[int(turn)]] += 1
+						elif move == [1, 0]:
+							ob["human_altruistic_" + turns[int(turn)]] += 1
+							ob["AI_altruistic_" + turns[int(turn)]] += 1
+							ob["non_coordination_altruistic_" + turns[int(turn)]] += 1
+						elif move == [1, 1]:
+							ob["human_altruistic_" + turns[int(turn)]] += 1
+							ob["AI_selfish_" + turns[int(turn)]] += 1
+							ob["coordination_altruistic_" + turns[int(turn)]] += 1
+
+				for strategy in strategies:
+					for player in players:
+						cum = 0
+						for turn in turns:
+							cum += ob[player + "_" + strategy +  "_" + turn]
+
+						ob[player + "_" + strategy +  "_cum"] = cum
+
+						avg = []
+						for turn in turns:
+							avg.append(ob[player + "_" + strategy +  "_" + turn])
+
+						ob[player + "_" + strategy +  "_average"] = np.mean(avg)
+
+					for event in events:
+						cum = 0
+						for turn in turns:
+							cum += ob[event + "_" + strategy +  "_" + turn]
+
+						ob[event + "_" + strategy +  "_cum"] = cum
+
+						avg = []
+						for turn in turns:
+							avg.append(ob[event + "_" + strategy +  "_" + turn])
+
+						ob[event + "_" + strategy +  "_average"] = np.mean(avg)
+
+				if i == 0:
+					writer.writerow(ob.keys())
+				writer.writerow(ob.values())
+				i += 1
+
+def pd():
+
+	with open('pd.pickle', 'rb') as handle:
+		    b = pickle.load(handle)
+
+	i = 0
+	with open('pd.csv', 'w') as f:
+		writer = csv.writer(f)
+
+		idList = [""]
+		for value in b:
+			ob = {}
+
+			ob["humanID"] = value["surveyID"]
+			ob["opponent"] = value["model"] 
+
+
+			if (ob["humanID"] not in idList):
+
+				idList.append(ob["humanID"])
+
+				if value["opponent"] == "Riley":
+					ob["condition"] = "human"
+				elif value["opponent"] == "AI": 
+					ob["condition"] = "ai"
+
+				gameState = json.loads(value["gameState"])
+
+
+
+				players = ["human", "AI"]
+				strategies = ["selfish", "altruistic"]
+				events = ["coordination", "non_coordination"]
+				turns = ["A", "B", "C"]
+
+				for turn in turns:
+					for strategy in strategies:
+						for player in players:
+							key = player + "_" + strategy +  "_" + turn
+							ob[key] = 0
+
+						#treat selfish coordination as when the human subject benefits from the AI
+						for event in events:
+							key = event + "_" + strategy + "_" + turn
+							ob[key] = 0
+
+				for turn, history in gameState.items():
+					moves = history.values()
+
+					for move in moves:
+						move_human = move[0]
+						move_AI = move[1]
+
+						if move == [0, 0]:
+							ob["human_altruistic_" + turns[int(turn)]] += 1
+							ob["AI_altruistic_" + turns[int(turn)]] += 1
+							ob["coordination_altruistic_" + turns[int(turn)]] += 1
+						elif move == [0, 1]:
+							ob["human_altruistic_" + turns[int(turn)]] += 1
+							ob["AI_selfish_" + turns[int(turn)]] += 1
+							ob["non_coordination_altruistic_" + turns[int(turn)]] += 1
+						elif move == [1, 0]:
+							ob["human_selfish_" + turns[int(turn)]] += 1
+							ob["AI_altruistic_" + turns[int(turn)]] += 1
+							ob["non_coordination_selfish_" + turns[int(turn)]] += 1
+						elif move == [1, 1]:
+							ob["human_selfish_" + turns[int(turn)]] += 1
+							ob["AI_selfish_" + turns[int(turn)]] += 1
+							ob["coordination_selfish_" + turns[int(turn)]] += 1
+
+				for strategy in strategies:
+					for player in players:
+						cum = 0
+						for turn in turns:
+							cum += ob[player + "_" + strategy +  "_" + turn]
+
+						ob[player + "_" + strategy +  "_cum"] = cum
+
+						avg = []
+						for turn in turns:
+							avg.append(ob[player + "_" + strategy +  "_" + turn])
+
+						ob[player + "_" + strategy +  "_average"] = np.mean(avg)
+
+					for event in events:
+						cum = 0
+						for turn in turns:
+							cum += ob[event + "_" + strategy +  "_" + turn]
+
+						ob[event + "_" + strategy +  "_cum"] = cum
+
+						avg = []
+						for turn in turns:
+							avg.append(ob[event + "_" + strategy +  "_" + turn])
+
+						ob[event + "_" + strategy +  "_average"] = np.mean(avg)
+
+				if i == 0:
+					writer.writerow(ob.keys())
+				writer.writerow(ob.values())
+				i += 1
+
+
+def separate():
+
+	bos = []
+	pd = []
+	hawkdove = []
+
+	with open('allData.pickle', 'rb') as handle:
+		b = pickle.load(handle)
+
+	for x in b:
+		el = b[x]
+		if int(x) > 10:
+			if el["game_type"] == "bos":
+				bos.append(el)
+			elif el["game_type"] == "pd":
+				pd.append(el)
+			elif el["game_type"] == "hawkdove":	
+				hawkdove.append(el)
+
+	# print(len(bos), len(pd), len(hawkdove))
+	with open('bos.pickle', 'wb') as bos_f:
+		pickle.dump(bos, bos_f)
+		print("success")
+
+	with open('pd.pickle', 'wb') as pd_f:
+		pickle.dump(pd, pd_f)
+		print("success")
+
+	with open('hawkdove.pickle', 'wb') as hawkdove_f:
+		pickle.dump(hawkdove, hawkdove_f)
+		print("success")
+
+pd()
